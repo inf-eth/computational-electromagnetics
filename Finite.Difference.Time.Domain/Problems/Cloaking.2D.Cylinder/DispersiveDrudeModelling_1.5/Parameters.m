5bbd23dee6ca974d14d3a02b854731758253464e
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2011-10-24 13:45:44 +0000

    - Files for modelling of 2D cylindrical cloak using a Drude model. Reference: A radially-dependent dispersive finite-differencetime-domain method for the evaluation of electromagnetic cloaks.pdf

---
+++ Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DispersiveDrudeModelling_1.5/Parameters.m
@@ -0,0 +1,8 @@
+function [Size XCenter YCenter delta ra rb] = Parameters
+% Simulation related parameters.
+Size = 100;
+XCenter = (Size+1)/2;
+YCenter = (Size-1)/2;
+delta = 1e-3;
+ra = 0.10;
+rb = 0.20;
