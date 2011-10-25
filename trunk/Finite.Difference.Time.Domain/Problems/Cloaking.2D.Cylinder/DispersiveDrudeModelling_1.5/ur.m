a46c8027d817f60e15b478c5b47c9a180129eae4
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2011-10-25 04:00:29 +0000

    - Generalized the 2D cylinder. Need to resolve n+1 and n+2 time indices in main computational loop.

--- trunk/Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DispersiveDrudeModelling_1.5/ur.m
+++ trunk/Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DispersiveDrudeModelling_1.5/ur.m
@@ -4,7 +4,7 @@ function return_val = ur ( i, j )
 [Size XCenter YCenter delta ra rb] = Parameters;
 
 b = rb;
-a = ra-0.003;
+a = ra;
 x = (i-XCenter) * delta;
 y = (j-YCenter) * delta;
 r = sqrt ( x^2 + y^2 );
@@ -17,7 +17,7 @@ if (i-XCenter)^2+(j-YCenter)^2 < (rb/delta)^2 & (i-XCenter)^2+(j-YCenter)^2 > (r
 %     %uyy = (r/(r-a)) + ( (a^2-2*a*r)/((r-a)*r^3) )*y^2;
 %     %uzz = (b/(b-a))^2 * ((r-a)/r);
     u = ((r-a)/r)^2;
-%     %return_val = [uxx uxy; uyx uyy];
+%    %return_val = [uxx uxy; uyx uyy];
     %return_val = [u 0; 0 u];
     return_val = [1 0; 0 1];
 else
