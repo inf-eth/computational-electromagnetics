81949942d87cf5c1afdd91e3947a367794e66746
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2011-10-25 07:54:34 +0000

    - Working generic simulation. Need to test on a better system which can handle 1000 x 1000 matrices.

--- Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DispersiveDrudeModelling_1.5/er.m
+++ Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DispersiveDrudeModelling_1.5/er.m
@@ -8,7 +8,7 @@ a = ra;
 x = (i-XCenter) * delta;
 y = (j-YCenter) * delta;
 r = sqrt ( x^2 + y^2 );
-offset = delta * 0;
+offset = delta * 3;
 
 if (i-XCenter)^2+(j-YCenter)^2 < ((rb/delta))^2 
     
@@ -18,10 +18,10 @@ if (i-XCenter)^2+(j-YCenter)^2 < ((rb/delta))^2
 %     %eyy = (r/(r-a)) + ( (a^2-2*a*r)/((r-a)*r^3) )*y^2;
     if (i-XCenter)^2+(j-YCenter)^2 > ((ra/delta)+offset)^2
         ezz = (b/(b-a))^2;
-        %return_val = ezz;
-        return_val = 1;
+        return_val = ezz;
+        %return_val = 1;
     else
-        return_val = 1;%e10;
+        return_val = 5;%1e10;
     end
 %     return_val = 1;
 else
--- Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DispersiveDrudeModelling_1.5/ur.m
+++ Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DispersiveDrudeModelling_1.5/ur.m
@@ -4,7 +4,7 @@ function return_val = ur ( i, j )
 [Size XCenter YCenter delta ra rb] = Parameters;
 
 b = rb;
-a = ra;
+a = ra - 3*delta;
 x = (i-XCenter) * delta;
 y = (j-YCenter) * delta;
 r = sqrt ( x^2 + y^2 );
@@ -18,8 +18,8 @@ if (i-XCenter)^2+(j-YCenter)^2 < (rb/delta)^2 & (i-XCenter)^2+(j-YCenter)^2 > (r
 %     %uzz = (b/(b-a))^2 * ((r-a)/r);
     u = ((r-a)/r)^2;
 %    %return_val = [uxx uxy; uyx uyy];
-    %return_val = [u 0; 0 u];
-    return_val = [1 0; 0 1];
+    return_val = [u 0; 0 u];
+    %return_val = [1 0; 0 1];
 else
     return_val = [1 0; 0 1];
 end
