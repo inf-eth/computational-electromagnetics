de380c9aacf3272066459582c2a3dae56cacfd8c
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2011-10-29 02:01:59 +0000

    - Forked a separate folder DrudeConversionShneider_1.7 from DrudeConversion_1.6. This follows the generalized Drude modelling procedure outlined in chapter 10 (10.4-5) from Shneider.

---
+++ trunk/Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DrudeConversionShneider_1.7/uphi.m
@@ -0,0 +1,24 @@
+% uphi calculation for cloaking problem.
+function return_val = uphi ( i, j )
+
+[Size XCenter YCenter delta ra rb] = Parameters;
+
+offset = delta * 0;
+b = rb;
+a = ra + offset;
+x = (i-XCenter) * delta;
+y = (j-YCenter) * delta;
+r = sqrt ( x^2 + y^2 );
+
+if (i-XCenter)^2+(j-YCenter)^2 < ((b/delta))^2 
+    
+    if (i-XCenter)^2+(j-YCenter)^2 > ((a/delta))^2
+        uphi = r/(r-a);
+        return_val = uphi;
+%         return_val = 1;
+    else
+        return_val = 1;%e80;
+    end
+else
+    return_val = 1;
+end
