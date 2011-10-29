5b777f71064fafdbd177efcfaa33bd047ee5502c
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2011-10-29 08:07:42 +0000

    - Separate folder for Drude Model Template.

---
+++ Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/2DDrudeModelTemplate/wpsquared.m
@@ -0,0 +1,8 @@
+% wpsquared calculation for cloaking problem.
+function return_val = wpsquared( i, j, w )
+
+[Size XCenter YCenter delta ra rb] = Parameters;
+
+A = rb/(rb-ra);
+
+return_val = w^2*(1-(ezz(i, j)/A));
