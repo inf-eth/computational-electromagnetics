48f91ba428f347f3b515da7909510de12a543950
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2011-10-30 04:05:46 +0000

    - Ideal case improvements. Need to implement PML (perfectly matched layer) or a better ABC (absorbing boundary condition) in order to reduce the effect of reflections.

--- trunk/Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DrudeConversion_1.6/ezz.m
+++ trunk/Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DrudeConversion_1.6/ezz.m
@@ -10,7 +10,7 @@ y = (j-YCenter) * delta;
 r = sqrt ( x^2 + y^2 );
 offset = delta * 0;
 
-A = b/(b-a);
+% A = b/(b-a);
 ezz = ((b/(b-a))^2) * ((r-a)/r);    % Ideal.
 % ezz = (b/(b-a))^2;  % Pendry reduced
 % ezz = b/(b-a); % Zhao reduced.
@@ -21,8 +21,8 @@ if (i-XCenter)^2+(j-YCenter)^2 < ((rb/delta))^2
     if (i-XCenter)^2+(j-YCenter)^2 > ((ra/delta)+offset)^2
         return_val = ezz;        
     else
-        return_val = A;%e80;
+        return_val = 1;%e80;
     end
 else
-    return_val = A;
+    return_val = 1;
 end
--- trunk/Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DrudeConversion_1.6/wpsquared.m
+++ trunk/Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DrudeConversion_1.6/wpsquared.m
@@ -3,6 +3,4 @@ function return_val = wpsquared( i, j, w )
 
 [Size XCenter YCenter delta ra rb] = Parameters;
 
-A = rb/(rb-ra);
-
-return_val = w^2*(1-(ezz(i, j)/A));
+return_val = w^2*(1-(ezz(i, j)/A(i, j)));
