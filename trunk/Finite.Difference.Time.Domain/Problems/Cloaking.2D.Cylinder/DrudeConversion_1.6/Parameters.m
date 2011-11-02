9592afdcafeed4bb006e79406e7aaae1cb10bddf
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2011-11-02 00:32:14 +0000

    - Phase I of PML implementation: Setting up PML thickness and incorporating it into general algorithm. - gamma/collision frequencies correction for DrudeConversion_1.6.

--- trunk/Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DrudeConversion_1.6/Parameters.m
+++ trunk/Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DrudeConversion_1.6/Parameters.m
@@ -1,4 +1,4 @@
-function [Size XCenter YCenter delta ra rb DT] = Parameters
+function [Size XCenter YCenter delta ra rb DT PMLw] = Parameters
 % Simulation related parameters.
 Size = 250;
 XCenter = (Size+1)/2;
@@ -9,3 +9,5 @@ ra = 0.1;
 rb = 0.2;
 Cl = 299792458;
 DT = delta / (sqrt(2) * Cl);
+
+PMLw = 5;   % Width or thickness of PML layer
---
+++ trunk/Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DrudeConversion_1.6/gammacolm.m
@@ -0,0 +1,3 @@
+% magnetic collision frequency calculation for cloaking problem.
+function return_val = gammacolm ( i, j )
+return_val = 0;
