67884d021970be868ff33b97d41e470a6c79737c
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2011-11-02 00:32:14 +0000

    - Phase I of PML implementation: Setting up PML thickness and incorporating it into general algorithm. - gamma/collision frequencies correction for DrudeConversion_1.6.

---
+++ Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DrudeConversion_1.6/gammacol.m
@@ -0,0 +1,3 @@
+% collision frequency calculation for cloaking problem.
+function return_val = gammacol ( i, j )
+return_val = 0;
--- Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DrudeConversion_1.6/wpmsquared.m
+++ Finite.Difference.Time.Domain/Problems/Cloaking.2D.Cylinder/DrudeConversion_1.6/wpmsquared.m
@@ -6,7 +6,7 @@ function return_val = wpmsquared( i, j, w )
 sinwDT = sin ( w * DT / 2 );
 coswDT = cos ( w * DT / 2 );
 
-return_val = ( 2*sinwDT * ( -2*(urr(i, j)-1)*sinwDT - urrdp(i, j)*gamma(i, j)*DT*coswDT ) ) / ( (DT^2)*(coswDT^2) );
+return_val = ( 2*sinwDT * ( -2*(urr(i, j)-1)*sinwDT - urrdp(i, j)*gammacol(i, j)*DT*coswDT ) ) / ( (DT^2)*(coswDT^2) );
 
 % return_val = w^2*(1-(urr(i, j)));
 % return_val = 0;
