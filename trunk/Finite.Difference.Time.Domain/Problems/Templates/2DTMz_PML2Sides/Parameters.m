3dec7baedfe1938468fdb26874e184375401a56c
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2011-11-05 11:46:05 +0000

    - Need to find out why waves are reflecting back and not propagating into PML medium.

--- trunk/Finite.Difference.Time.Domain/Problems/Templates/2DTMz_PML2Sides/Parameters.m
+++ trunk/Finite.Difference.Time.Domain/Problems/Templates/2DTMz_PML2Sides/Parameters.m
@@ -2,7 +2,7 @@
 
 function [ISize JSize XCenter YCenter delta ra rb DT PMLw] = Parameters
 
-ISize = 200;
+ISize = 100;
 JSize = 100;
 XCenter = (ISize+1)/2;
 YCenter = (JSize-1)/2;
@@ -12,4 +12,4 @@ ra = 0.1;
 rb = 0.2;
 Cl = 299792458;
 DT = delta / (sqrt(2) * Cl);
-PMLw = 7;   % PML width. It is automatically assumed that outermost PML layer is PEC. Effectively PML is (PMLw - 1) cells wide.
+PMLw = 11;   % PML width. It is automatically assumed that outermost PML layer is PEC. Effectively PML is (PMLw - 1) cells wide.
