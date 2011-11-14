a2e507a8d0a52823733e705a376149dd8fce9c75
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2011-11-14 14:07:21 +0000

    - FDTD1D simulation in C++. Fields are saved as numbered files with extension .fdt - Matlab script to display field simulation. It reads in .fdt files and runs a movie. - * Need to make the code portable for linux. - * Think of a better way to load/store fields, most preferably using fwrite and fread.

---
+++ trunk/Finite.Difference.Time.Domain/Problems/FDTD.1D.Wave.Propagation.Cpp/FDTD1DWavePropagation.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+
+typedef unsigned int uint;
+
+// Simulation parameters.
+const uint w = 1024;			// No. of spatial steps
+const uint TimeN = 1024;		// No. of time steps
+const double imp0 = 377.0;		// Impedence of free space
+
+// Data Arrays.
+double *Ez = new double[w*TimeN]; // z-component of E-field
+double *Hy = new double[w*TimeN]; // y-component of H-field
+
+int main (int argc, char **argv)
+{
+	// File handling from chapter 3 of Understanding FDTD. J. B. Schneider
+	char basename[20] = "../../FieldData/Ez";
+	char filename[30];
+	uint frame = 1;
+	FILE *snapshot;
+	uint SnapshotResolutiont = 1;
+	bool SaveFields = true;
+
+	// Initialization.
+	for (uint z = 0; z < (w*TimeN); z++)
+	{
+		Ez[z] = 0;
+		Hy[z] = 0;
+	}
+
+	uint i, t;
+	clock_t start, end;
+	start = clock();
+	for (t = 1; t < TimeN; t++)
+	{
+		// Hy
+		for (i=0; i < (w-1); i++)
+		{
+			Hy[i+t*w] = Hy[i+(t-1)*w] + ( (Ez[i+1+(t-1)*w] - Ez[i+(t-1)*w])/imp0 );
+		}
+		// Ez
+		for (i=1; i<w; i++)
+		{
+			Ez[i+t*w] = Ez[i+(t-1)*w] + ( (Hy[i+t*w] - Hy[i-1+t*w])*imp0 );
+		}
+		Ez[0+t*w] = Ez[0+t*w] + exp ( -1 * pow((t-30.), 2)/100 );
+
+		// Write Ez snapshot.
+		if (t%SnapshotResolutiont == 0 && SaveFields == true)
+		{
+			sprintf_s (filename, "%s%d.fdt", basename, frame);
+			fopen_s (&snapshot, filename, "w");
+			for (i=0; i<w; i++)
+			{
+				fprintf_s (snapshot, "%g\n", Ez[i+t*w]);
+			}
+			fclose (snapshot);
+			frame++;
+		}
+	}
+	end = clock();
+	std::cout << "Time elapsed: " << (double)(end-start)/CLOCKS_PER_SEC << " seconds." << std::endl;
+	return 0;
+}
