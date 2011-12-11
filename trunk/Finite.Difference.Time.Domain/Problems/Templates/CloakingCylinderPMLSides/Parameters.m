078001303d8a28a1249143c83e3c6770b9baf7de
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2011-12-11 14:08:10 +0000

    - Introduced dtscalar for cloaking cylinder simulation with PML.

--- trunk/Finite.Difference.Time.Domain/Problems/Templates/CloakingCylinderPMLSides/Parameters.m
+++ trunk/Finite.Difference.Time.Domain/Problems/Templates/CloakingCylinderPMLSides/Parameters.m
@@ -1,6 +1,6 @@
 % Simulation related parameters.
 
-function [ISize JSize XCenter YCenter delta ra rb DT PMLw] = Parameters
+function [ISize JSize XCenter YCenter delta ra rb DT PMLw dtscalar] = Parameters
 
 ISize = 200;
 JSize = 200;
@@ -11,5 +11,6 @@ delta = 2.5e-3;
 ra = 0.1;
 rb = 0.2;
 Cl = 299792458;
-DT = delta / (sqrt(2) * Cl);
+dtscalar = 2;
+DT = delta / (sqrt(2) * Cl) / dtscalar;
 PMLw = 50;   % PML width. It is automatically assumed that outermost PML layer is PEC. Effectively PML is (PMLw - 1) cells wide.
