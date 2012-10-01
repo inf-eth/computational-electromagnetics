9a447c7b9ebe027f1e0adc40d678897af661fa13
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2012-10-01 14:26:29 +0000

    [FDTD] - A few corrections to notations.

--- trunk/Finite.Difference.Time.Domain/Problems/FDTD.1D.Wave.Propagation/One_Dimension_Wave_Propagation_Ex_Hy.m
+++ trunk/Finite.Difference.Time.Domain/Problems/FDTD.1D.Wave.Propagation/One_Dimension_Wave_Propagation_Ex_Hy.m
@@ -1,8 +1,9 @@
 % Simulation parameters.
-SIZE = 512; % No. of spatial steps
-MaxTime = 512; % No. of time steps
-PulseWidth = 128; % Width of Gaussian Pulse
+SIZE = 128; % No. of spatial steps
+MaxTime = 128; % No. of time steps
+PulseWidth = 32; % Width of Gaussian Pulse
 imp0 = 377.0; % Impedence of free space
+source = 2; % Location of source
 
 % Constants.
 c = 3e8;
@@ -10,12 +11,12 @@ pi = 3.141592654;
 e0 = (1e-9)/(36*pi);
 u0 = (1e-7)*4*pi;
 
-dt = 1e-12;
+dt = 1.0e-12;
 dz = 3e-4;
 Sc = c * dt/dz
 
 % Initialization.
-Ex = zeros ( SIZE, MaxTime ); % z-component of E-field
+Ex = zeros ( SIZE, MaxTime ); % x-component of E-field
 Hy = zeros ( SIZE, MaxTime ); % y-component of H-field
 PLOT1(1) = 0; % Data for plotting.
 
@@ -27,17 +28,20 @@ for q = 2:MaxTime
     % ABC for H at SIZE.
     Hy(SIZE,q) = Hy(SIZE-1,q-1) + (Sc-1)/(Sc+1)*(Hy(SIZE-1,q) - Hy(SIZE,q-1) );
     
-    % Calculation of Ez using updated difference equation for Ez. This is time step q+1/2.
+    % Calculation of Ex using updated difference equation for Ex. This is time step q+1/2.
     Ex(2:SIZE,q) = Ex(2:SIZE, q-1) + ( dt/(e0*dz)*(Hy(1:SIZE-1, q) - Hy(2:SIZE, q)) );
     % ABC for E at 1.
     Ex(1,q) = Ex(2,q-1) + (Sc-1)/(Sc+1)*(Ex(2,q) - Ex(2,q-1));
     
     % Activating a plane-wave source.
-    Ex(100,q) = Ex(100,q) + exp( -1*(q-PulseWidth)^2/PulseWidth ) * Sc;
+    Ex(source,q) = Ex(source,q) + exp( -1*((q-PulseWidth)^2)/PulseWidth ) * Sc;
 end
 toc
 % Simulation animation.
 for i=1:MaxTime
     figure (2)
     plot ( Ex(:,i) )
+    axis([0 SIZE -0.2 1])
+    xlabel('Spatial step (k)')
+    ylabel('Electric field (Ex)')
 end
