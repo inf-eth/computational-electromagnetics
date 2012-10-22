34b988ce22697d49cb2ba462af6107b2f649d183
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2012-10-22 05:21:36 +0000

    - FDTD1D Cpp: Improvements to drawing script.

--- trunk/Finite.Difference.Time.Domain/Problems/FDTD.1D.Wave.Propagation.Cpp/FieldPlot.m
+++ trunk/Finite.Difference.Time.Domain/Problems/FDTD.1D.Wave.Propagation.Cpp/FieldPlot.m
@@ -7,18 +7,24 @@ simTime = 255;
 frame = 1;
 figure(1);
 i = 0;
+data = 1:1024;
+figure(1)
+plothandle = plot (data);
+axis ([0 length(data) y_min y_max]);
 while i < simTime
     filename = sprintf ('%s%d.fdt', basename, frame);
     fid = fopen (filename, 'rt');
     if fid == -1
         return;
     end
-    data = fscanf (fid, '%f');
+    data = fread (fid, 1024, 'double');
     fclose (fid);
-    plot (data)
-    axis ([0 length(data) y_min y_max]);
-    reel (frame) = getframe;
+    %plot (data)
+    drawnow
+    set(plothandle, 'YData', data(:,1));
+    %reel (frame) = getframe;
     frame = frame+1;
     i = i+1;
+    pause(0.01)
 end
 %movie (reel, 1);
