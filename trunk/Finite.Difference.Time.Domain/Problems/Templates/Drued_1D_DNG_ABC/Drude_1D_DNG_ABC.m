7438b46f5bbe32a54c6f02e0d9eed29da7539943
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2012-10-25 05:22:01 +0000

    [Drude 1D DNG] - Corrected relations for field and flux densities. * Proper selection of wp and wpm should result in DNG material.

--- trunk/Finite.Difference.Time.Domain/Problems/Templates/Drued_1D_DNG_ABC/Drude_1D_DNG_ABC.m
+++ trunk/Finite.Difference.Time.Domain/Problems/Templates/Drued_1D_DNG_ABC/Drude_1D_DNG_ABC.m
@@ -2,7 +2,7 @@ clc
 clear all
 
 % Simulation parameters.
-SIZE = 64; % No. of spatial steps
+SIZE = 128; % No. of spatial steps
 SlabLeft = round(SIZE/3); % Location of left end of Slab.
 SlabRight = round(2*SIZE/3); % Location of right end of Slab
 MaxTime = SIZE; % No. of time steps
@@ -30,11 +30,17 @@ Dx = zeros(SIZE, 3); % x-component of D
 Hy = zeros(SIZE, 3); % y-component of H-field
 By = zeros(SIZE, 3); % y-component of B
 
-wpsq = zeros(1,SIZE);
+wpsq = zeros(SIZE,1);
 wpsq(SlabLeft:SlabRight) = 2*w^2;
-wpmsq = zeros(1,SIZE);
+wpmsq = zeros(SIZE,1);
 wpmsq(SlabLeft:SlabRight) = 2*w^2;
-size(wpsq)
+
+a = 4./(e0*(4+wpsq*dt^2));
+b = -1*e0*a;
+c = -1*(wpsq*dt^2)./(4+(wpsq*dt^2));
+am = 4./(u0*(4+wpmsq*dt^2));
+bm = -1*u0*am;
+cm = -1*(wpmsq*dt^2)./(4+(wpmsq*dt^2));
 
 ExSnapshots = zeros(SIZE, MaxTime); % Data for plotting.
 frame = 1;
@@ -53,14 +59,16 @@ for q = 2:MaxTime
     
     % Calculation of Hy using update difference equation for Hy. This is time step q.
     By(1:SIZE-1,n2) = By(1:SIZE-1,n1) + ( ( Ex(1:SIZE-1,n1) - Ex(2:SIZE,n1) ) * dt/(dz) );
-    Hy(:,n2) = (1/u0)*By(:,n2);
+    Hy(:,n2) = am.*(By(:,n2)-2*By(:,n1)+By(:,3))+bm.*(-2*Hy(:,n1)+Hy(:,3))+cm.*(2*Hy(:,n1)+Hy(:,3));
+    
     % ABC for H at SIZE.
     Hy(SIZE,n2) = Hy(SIZE-1,n1) + (Sc-1)/(Sc+1)*(Hy(SIZE-1,n2) - Hy(SIZE,n1) );
     By(SIZE,n2) = u0*Hy(SIZE,n2);
 
     % Calculation of Ex using updated difference equation for Ex. This is time step q+1/2.
     Dx(2:SIZE,n2) = Dx(2:SIZE, n1) + ( dt/(dz)*(Hy(1:SIZE-1, n2) - Hy(2:SIZE, n2)) );
-    Ex(:,n2) = (1/e0)*Dx(:,n2);
+    Ex(:,n2) = a.*(Dx(:,n2)-2*Dx(:,n1)+Dx(:,3))+b.*(-2*Ex(:,n1)+Ex(:,3))+c.*(2*Ex(:,n1)+Ex(:,3));
+    
     % ABC for E at 1.
     Ex(1,n2) = Ex(2,n1) + (Sc-1)/(Sc+1)*(Ex(2,n2) - Ex(2,n1));
     Dx(1,n2) = e0*Ex(1,n2);
@@ -79,7 +87,7 @@ end
 toc
 % Simulation animation.
 for i=1:frame-1
-    figure (2)
+    figure (1)
     plot ( ExSnapshots(:,i) )
     axis([0 SIZE -0.6 0.6])
     xlabel('Spatial step (k)')
