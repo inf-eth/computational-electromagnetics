623f46723213d68958beec53a306288a15076dc1
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2012-11-10 02:24:00 +0000

    [Drude 1D DNG] - C++ implementation initial files.

---
+++ Finite.Difference.Time.Domain/Problems/Drude_1D_DNG_Transmission_Coefficient/Cpp/include/FDTD1DDNG.h
@@ -0,0 +1,76 @@
+// Constants.
+#define c0	299792458.
+#define pi	3.14159265358979323846
+
+class CFDTD1DDNG
+{
+private:
+	// Simulation parameters.
+	const unsigned int Size;
+	const unsigned int MaxTime;
+	const unsigned int PulseWidth;
+	const unsigned int td;
+	const unsigned int SourceLocation;
+	const unsigned int SlabLeft;
+	const unsigned int SlabRight;
+	const unsigned int SnapshotInterval;
+	const double e0;
+	const double u0;
+	const double dt;
+	const double dz;
+	const double Sc;
+
+	// Frequency, wavelength, wave number.
+	const double l;
+	const double f;
+	const double fmax;
+	const double w;
+	const double k0;
+
+	// Choice of source.
+	// 1. Gaussian pulse 2. Sine wave 3. Ricker wavelet
+	const unsigned int SourceChoice;
+	const double fp; // Ricker wavelet peak frequency.
+	const double dr; // Ricker wavelet delay.
+
+	// Data arrays.
+	double *Ex;
+	double *Dx;
+	double *Hy;
+	double *By;
+	double *ExSnapshots;
+	unsigned int frame;
+
+	// Incident and transmitted fields.
+	double *Exi;
+	double *Ext;
+	double *Extt;
+	const unsigned int x1;
+
+	// Refractive index.
+	const unsigned int Z1;
+	const double z1;
+	const unsigned int Z2;
+	const double z2;
+	double *Exz1;
+	double *Exz2;
+
+	// Drude material parameters.
+	double *einf;
+	double *uinf;
+	double *wpesq;
+	double *wpmsq;
+	double *ge;
+	double *gm;
+
+	// Auxiliary field scalars.
+	double *ae0, *ae, *be, *ce, *de, *ee;
+	double *am0, *am, *bm, *cm, *dm, *em;
+
+	// Time indices;
+	unsigned int n0, n1;
+
+public:
+	CFDTD1DDNG();
+	~CFDTD1DDNG();
+};
