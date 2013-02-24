315b922106b84ef7eb20097bed452b6f7d880f38
infinity.ethereal <infinity.ethereal@0d88c5ae-8e33-f000-d3c1-fbb3733563b2>
2013-02-24 07:10:21 +0000

    [Cloaking Cylinder PML Sides] - Modified sinphi formula. - Improvements to progress indicator.

--- trunk/Finite.Difference.Time.Domain/Problems/Templates/CloakingCylinderPMLSides/sinphi.m
+++ trunk/Finite.Difference.Time.Domain/Problems/Templates/CloakingCylinderPMLSides/sinphi.m
@@ -1,12 +1,10 @@
 % sinphi calculation for 2D cloaking problem.
-function return_val = sinphi ( i, j )
+function return_val = sinphi(i, j)
 
-[ISize JSize XCenter YCenter delta ra rb DT PMLw] = Parameters;
+[pI pJ pPMLw pra prb pdt pdelta px0 py0] = Parameters;
 
-b = rb;
-a = ra;
-x = (i-XCenter) * delta;
-y = (j-YCenter) * delta;
-r = sqrt ( x^2 + y^2 );
+x = (i*delta) - px0;
+y = (j*delta) - py0;
+r = sqrt(x^2 + y^2);
 
 return_val = y/r;
